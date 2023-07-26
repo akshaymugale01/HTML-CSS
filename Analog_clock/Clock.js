9837dec1e13ea4a97c30aec09644302160fa008c
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2023-07-26 13:14:35 +0530

    Add files via upload

---
+++ Analog_clock/Clock.js
@@ -0,0 +1,44 @@
+/*12hour=360deg
+1hour=360/12=30
+h hour= 30h + (1/2)m
+......
+1hr=60min 
+min*
+60min=30
+1min=30/60 =(1/2)m
+
+for min**
+60min=360deg
+1min= 360/60=6deg
+m min=6m
+
+for sec**
+1min=60sec
+60sec=360deg
+1sec=360/60=6deg
+s sec=6s */
+
+
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+//gettng hour, min sec from date
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation =30*hh + mm/2;
+    let mRotation =6*mm;
+    let sRotation =6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+
+}
+
+setInterval(displayTime, 1000);
